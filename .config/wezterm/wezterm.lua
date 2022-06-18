cbdc1c2974f455c8650d6136f4cb0e18b6e704a6
Charly Delay <0xcharly@users.noreply.github.com>
2022-06-18 11:26:02 +0900

    Adjust tab bar and style support

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,5 @@
 local wezterm = require 'wezterm'
-local colorscheme = require 'colors.github_dark'
-
--- The filled in variant of the < symbol
-local SLANTED_LEFT = utf8.char(0xe0ba)
-
--- The filled in variant of the > symbol
-local SLANTED_RIGHT = utf8.char(0xe0b8)
+local colorscheme = require 'colors.github_dark_dimmed'
 
 wezterm.on('format-tab-title',
            function(tab, tabs, panes, config, hover, max_width)
@@ -21,36 +15,59 @@ wezterm.on('format-tab-title',
     foreground = colorscheme.tab_bar.inactive_tab_hover.fg_color
   end
 
-  local edge_foreground = background
-
   -- ensure that the titles fit in the available space,
   -- and that we have room for the edges.
-  local title = wezterm.truncate_right(tab.active_pane.title, max_width - 4)
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
 
   return {
     {
       Background = {
         Color = (tab.tab_index == 0 and background or edge_background),
       },
-    }, {Foreground = {Color = edge_foreground}}, {Text = SLANTED_LEFT},
-    {Background = {Color = background}}, {Foreground = {Color = foreground}},
+    }, {Background = {Color = background}}, {Foreground = {Color = foreground}},
     {Text = ' ' .. title .. ' '}, {Background = {Color = edge_background}},
-    {Foreground = {Color = edge_foreground}}, {Text = SLANTED_RIGHT},
   }
 end)
 
 return {
+  -- Spawn a fish shell in login mode
+  default_prog = {'/opt/homebrew/bin/fish', '-l'},
+  term = 'wezterm',
+
+  -- Use Pragmata Pro with ligature and Nerd icons as main font.
+  -- Use Codicon for VSCode glyphs.
+  -- Use variations of the Pragmata Pro font (with less features) as fallbacks.
+  -- Use other fonts I like as fallback in case they are installed, but Pragmata
+  -- is not.
   font = wezterm.font_with_fallback({
-    'PragmataProLiga Nerd Font', 'PragmataPro Liga', 'PragmataPro', 'MonoLisa',
-    'Hack', 'Inconsolata',
+    'PragmataProLiga Nerd Font', 'codicon', 'PragmataPro Liga', 'PragmataPro',
+    'MonoLisa', 'Hack', 'Inconsolata',
   }),
+
+  -- Font size 16 for the best rendering.  Might be on the larger side at some
+  -- DPI/screen resolutions.
   font_size = 16,
-  line_height = 0.95,
+
+  -- Enable the tab bar, use simple look.
   enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
-  tab_max_width = 36,
+
+  -- No title bar, but allow resize.
   window_decorations = 'RESIZE',
 
+  -- Colorscheme load above and also used for the tab bar.
   colors = colorscheme,
+
+  -- Some common hyperlink rules.
+  hyperlink_rules = {
+    {regex = '\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b', format = '$0'},
+    {regex = '\\b\\w+://(?:[\\w.-]+)\\S*\\b', format = '$0'},
+    {regex = '\\bfile://\\S*\\b', format = '$0'},
+    {regex = '\\bb/(\\d+)\\b', format = 'https://b.corp.google.com/issues/$1'},
+    {
+      regex = '\\bcl/(\\d+)\\b',
+      format = 'https://critique.corp.google.com/issues/$1',
+    },
+  },
 }
